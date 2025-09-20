0550348036f332792e47c2bfe1349bad8ea03cec
HannesRuhe <hannes.ruhe@gmail.com>
2025-09-20 11:10:39 +0200

    IN-OUT-Check-V4

--- Hannes-Tests/Verarbeitung_Libary.py
+++ Hannes-Tests/Verarbeitung_Libary.py
@@ -14,11 +14,15 @@ def pruefe_in_out(transport_daten):
         zeitpunkt = Zeile[5]                   # Zeit ist im 6. Feld 
 
         if aktuell == status:
+            
             diff = zeitpunkt - letzte_zeit
+            minuten = diff.seconds // 60
+            sekunden = diff.seconds % 60
+
             if status == "in":
-                in_out_fehler.append(f"Doppelter Eincheck-Zeitpunkt, Abstand: {diff}")
+                in_out_fehler.append(f"Doppelter Eincheck-Zeitpunkt, Abstand: {minuten} Minuten {sekunden} Sekunden")
             if status == "out":
-                in_out_fehler.append(f"Doppelter Auscheck-Zeitpunkt, Abstand: {diff}")
+                in_out_fehler.append(f"Doppelter Auscheck-Zeitpunkt, Abstand: {minuten} Minuten {sekunden} Sekunden")
 
         if status == "in":
             in_out_aktuell = "Eingecheckt"
